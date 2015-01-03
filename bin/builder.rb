aa0ee4b3e8c4ca326c74ad91aa1764eaa8febe90
squidgetx <squidgetx@gmail.com>
2015-01-02 21:46:18 -0500

    d

--- bin/builder.rb
+++ bin/builder.rb
@@ -7,8 +7,8 @@ directives = {
   'container' => "<table class='container'><tr><td>",
   '/container' => "</td></tr></table>",
 
-  'header' => "<table class='row'><tr class='header'><td class='wrapper last'><table class='eight columns'><tr><td class='text-pad'>",
-  '/header' => "</td><td class='expander'></td></tr></table> </td> <td class='wrapper last'><table class='four columns'></table></td></tr></table>",
+  'header' => "<table class='row'><tr class='header'><td class='wrapper last'><table class='seven columns'><tr><td class='text-pad-lg'>",
+  '/header' => "</td><td class='expander'></td></tr></table> </td> <td class='wrapper last'><table class='five columns'></table></td></tr></table>",
 
   'subtitle' => "<table class='row'><tr><td class='wrapper offset-by-five last'><table class='seven columns'><tr><td><p class='inside subtitle'>",
   '/subtitle' => "</p>" + rowclose,
@@ -16,22 +16,24 @@ directives = {
   'quotebox' => "<table class='row'><tr><td class='wrapper last'><table class='nine columns'><tr class = 'quoteback'><td class='text-pad'><br />",
   '/quotebox' => rowclose,
 
-  'quote' => "<p><i>",
+  'quote' => "<p class='quoteback'><i>",
   '/quote' => "</i></p>",
 
-  'quotee' => "<p>&nbsp;&nbsp;&nbsp;&nbsp;- ",
+  'quotee' => "<p class='quoteback'>&nbsp;&nbsp;&nbsp;&nbsp;- ",
   '/quotee' => "</p>",
 
   'row' => "<table class='row'><tr><td class='wrapper last'><table class='twelve columns'><tr><td class='text-pad'>",
   '/row' => rowclose,
 
-  'footer' => "<table class='row'><tr class='signoff'><td class='wrapper last'><table class='twelve columns'><tr><td class='text-pad'><br />",
+  'footer' => "<table class='row'><tr class='signoff'><td class='wrapper'><table class='ten columns'><tr><td class='text-pad'><br />",
   '/footer' => rowclose,
 
   'signoff' => "<p class='signoff'>",
   '/signoff' => "</p>",
   'signature' => "<p class='signoff'>&nbsp;&nbsp;&nbsp; - ",
-  '/signature' => "</p>"
+  '/signature' => "</p></td></tr></table></td>",
+  'animal' => "<td class='wrapper last'><table class='two columns'><tr><td>",
+  '/animal' => "</td></tr></table></td>"
 }
 
 while (true)
