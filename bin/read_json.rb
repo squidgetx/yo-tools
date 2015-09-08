214fa2ec59a4dd44b0b0441cefa03b4d1167159c
Sylvan Zheng <squidgetx@gmail.com>
2015-09-08 16:49:32 -0400

    update template

--- bin/read_json.rb
+++ bin/read_json.rb
@@ -1,31 +1,8 @@
+#!/usr/bin/env ruby
+
 # Ruby script to facilitate the YO email sending
 # Takes a single argument; the email-in-question's json data
 # Specification of the email instion's json data:
-# email: {
-#   n: 1,
-#   subject: "subject field",
-#   interlude: "html message (optional field)",
-#   quote: "quotation",
-#   author: "quote author",
-#   cover: {
-#     image_url: "imgur link to cover photo",
-#     subtitle: "subtitle (optional field)",
-#   }
-#
-#   trips: [
-#     "Trip Title": {
-  #     signup_link: "signup link",
-  #     leaders: [
-  #       "Liz Karron": "elizabeth.karron@yale.edu",
-  #       "Marcus Russi": "marcus.russi@yale.edu"
-  #      ],
-#        date: "date",
-#        cost: "some cost",signup link",
-#        image_url: "imgur link",
-#        blurb: "text"
-#      }
-#    ]
-# }
 
 require 'json'
 require 'pry'
